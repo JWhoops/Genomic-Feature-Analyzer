0f4b7895a5b047426ef02d76028937946ef216a0
Wu <jwu@C07YH2UHJYW0.local>
2019-08-23 14:02:05 -0500

    finished file mapper

---
+++ file_map_generator.js
@@ -0,0 +1,53 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const path = require("path");
+const fs = require("fs");
+
+let files = [];
+const directoryPath = path.join(__dirname, "mamals");
+
+// promisefication
+new Promise(function(resolve, reject) {
+  fs.readdir(directoryPath, function(err, files) {
+    if (err) return console.log("Unable to scan directory: " + err);
+    files.forEach(function(file) {
+      files.push(file);
+    });
+    resolve(files);
+  });
+})
+  .then(result_files => {
+    files = result_files;
+    const pattern = /^GCF_[0-9]*.*/;
+    let mamal_urls = [];
+    const base_url = "https://www.ncbi.nlm.nih.gov/assembly/";
+    files = result_files.filter(file_name => pattern.test(file_name));
+    for (f of files) {
+      const assembly_acces = "GCF_" + f.split("_")[1];
+      const url = base_url + assembly_acces + "/";
+      mamal_urls.push(url);
+    }
+    let requests = mamal_urls.map(url => axios.get(url));
+    return Promise.all(requests);
+  })
+  .then(responses => {
+    let files_map = {};
+    for (let i = 0; i < responses.length; i++) {
+      files_map[files[i]] = get_name(responses[i].data);
+    }
+    files_map = JSON.stringify(files_map);
+    fs.writeFile("files_map.json", files_map, err => {
+      if (err) console.log(err);
+      console.log("Successfully Written to File.");
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+function get_name(html) {
+  const $ = cheerio.load(html);
+  return $("dl.assembly_summary_new dd a")
+    .first()
+    .text();
+}
