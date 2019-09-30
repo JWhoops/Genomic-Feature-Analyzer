752aa8eece0731d2ad8469732168f8823dfbef74
Wu <jwu@C07YH2UHJYW0.local>
2019-09-30 09:35:16 -0500

    wrote tests

--- test/test.js
+++ test/test.js
@@ -4,34 +4,34 @@ const calc = require("../exon_and_gene_calculator");
 describe("calculate gene", function() {
   describe("#calculate_file", function() {
     it("should return correct coded exon length: 458509", async function() {
-      let result = await calc.calculate_file("./mes/one_gene.gff");
+      let result = await calc.calculate_file("./test/test_gffs/one_gene.gff");
       assert.equal(result.average_exon_length, 458509);
     });
     it("should return correct coded exon length: 458509 with duplciation and pseudo gene", async function() {
       let result = await calc.calculate_file(
-        "./mes/one_gene_duplication_and_pseudp.gff"
+        "./test/test_gffs/one_gene_duplication_and_pseudp.gff"
       );
       assert.equal(result.average_exon_length, 458509);
     });
     it("should return correct coded exon length: 458509 with one pseudo gene", async function() {
-      let result = await calc.calculate_file("./mes/one_gene_with_pseudo.gff");
+      let result = await calc.calculate_file("./test/test_gffs/one_gene_with_pseudo.gff");
       assert.equal(result.average_exon_length, 458509);
     });
     it("should return correct coded exon length: 0 wihtout any encoded exon", async function() {
       let result = await calc.calculate_file(
-        "./mes/one_gene_no_coded_exon.gff"
+        "./test/test_gffs/one_gene_no_coded_exon.gff"
       );
       assert.equal(result.average_exon_length, 0);
     });
     it("should return correct coded exon length: 0 with illegal gene", async function() {
       let result = await calc.calculate_file(
-        "./mes/one_gene_no_coded_exon.gff"
+        "./test/test_gffs/one_gene_no_coded_exon.gff"
       );
       assert.equal(result.average_exon_length, 0);
     });
     it("should return correct coded exon length: 458509 with multiple same genes", async function() {
       let result = await calc.calculate_file(
-        "./mes/multiple_gene.gff"
+        "./test/test_gffs/multiple_gene.gff"
       );
       assert.equal(result.average_exon_length, 458509);
     });
